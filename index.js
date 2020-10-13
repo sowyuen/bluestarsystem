6d5314f6f783090ca31a79b64b052e2a3cacddb3
bernice <sowyuenl@gmail.com>
2020-10-13 16:00:56 +0800

    qpc.html, slide.html
    - change circle size
    - tried slideshow

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ window.onload=function(){
   autoplay:true,
   autoplaySpeed:1500,
   arrows:true,
-  prevArrow:'<button type="button" class="slick-prev"></button>',
-  nextArrow:'<button type="button" class="slick-next"></button>',
   centerMode:true,
   slidesToShow:1,
   slidesToScroll:1,
